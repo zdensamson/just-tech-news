e7da9db2befa57109836c24087787e1551edc5d6
Deku <zachdsamson@gmail.com>
2021-09-06 16:46:16 -0700

    created /login route

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -50,6 +50,29 @@ router.post('/', (req,res) => {
     })
 });
 
+// LOGIN
+router.post('/login', (req,res) => {
+    // expects {email: 'string', password: 'string'}
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!'});
+            return;
+        }
+        
+        // Verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!'});
+            return;
+        }
+        res.json({ user: dbUserData, message: 'You are now logged in!'});
+    });
+});
+
 // PUT /api/users/ID (UPDATE)
 router.put('/:id', (req,res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
