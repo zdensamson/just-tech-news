51207b35e01eca8eaef78229f099789ceab0dfdf
Deku <zachdsamson@gmail.com>
2021-09-10 17:00:09 -0700

    finished COMMENT routes

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require('./User');
 const Post = require('./Post');
 const Vote = require('./Vote');
+const Comment = require('./Comment');
 
 // create associations 
 // ONE-TO-MANY
@@ -42,6 +43,24 @@ User.hasMany(Vote, {
 
 Post.hasMany(Vote, {
     foreignKey: 'post_id'
-})
+});
 
-module.exports = { User, Post, Vote };
+
+// COMMENT associations
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote, Comment };
