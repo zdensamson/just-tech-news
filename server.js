fc3f3815d4cdfe6730abd56f98349e17794846a2
Deku <zachdsamson@gmail.com>
2021-09-19 14:45:47 -0700

    added helper functions to templates

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 
+const helpers = require('./utils/helpers');
+
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -19,7 +21,7 @@ const sess = {
 
 
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({helpers});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
